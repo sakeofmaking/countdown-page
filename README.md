19512c0a2f6f4d9bc42f0f01e66334c07c42f013
rducky <sakeofmaking@gmail.com>
2025-05-20 18:26:34 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Countdown
+
+### Description
+* Countdown timer
+* Made from HTML and CSS for use on GitHub Pages
+* Date is entered in format YYYY-MM-DDThh:mm:ss
+
---
+++ script.js
@@ -0,0 +1,48 @@
+// @source: https://stackoverflow.com/questions/41145086/js-countdown-timer-to-a-specific-date
+
+$(document).ready(function() {
+
+function getTimeRemaining(endtime) {
+  var t = endtime - new Date().getTime();
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+function initializeClock(id, endtime) {
+  var clock = document.getElementById(id);
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
+
+var count_to_str = document.getElementById('count_to').innerHTML.trim();
+var deadline = new Date(count_to_str);
+initializeClock('clockdiv', deadline);
+
+});
