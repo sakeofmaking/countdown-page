06e4ad0e604b87336718399e19ebd6b191361e2f
rducky <sakeofmaking@gmail.com>
2026-05-31 14:30:06 -0700

    Countdown to freedom

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
             $('#hours').text('00');
             $('#minutes').text('00');
             $('#seconds').text('00');
-            $('#message').text('GO TIME!');
+            $('#message').text('FREEDOM!');
             clearInterval(timeinterval);
             return;
         }
